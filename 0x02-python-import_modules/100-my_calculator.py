b88e83a7946a1455fb76f5c483a1e71ff97ed58b
Konyancha1 <kmuua791@gmail.com>
2021-11-22 14:07:04 +0200

    Made executable

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 if __name__ == '__main__':
     import sys
+    
     number_args = len(sys.argv) - 1
     if number_args != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
